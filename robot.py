445efbaf729bbb74a0a2198efe539ea7464adca7
alycia <alyciabergeron010108@gmail.com>
2022-11-07 17:56:08 -0500

    Added robot.py

---
+++ robot.py
@@ -0,0 +1,15 @@
+import wpilib
+import wpilib.drive
+class Robot(wpilib.TimedRobot):
+    def robotInit(self):
+        self.joystick = wpilib.Joystick(0)
+        self.motor_gauche = wpilib.VictorSP(0)
+        self.motor_droit = wpilib.VictorSP(1)
+        self.drive = wpilib.drive.DifferentialDrive(self.motor_gauche, self.motor_droit)
+
+    def teleopPeriodic(self):
+        self.drive.arcadeDrive(self.joystick.getY() * -1, self.joystick.getX())
+
+if __name__ == '__main__':
+    wpilib.run(Robot)
+
